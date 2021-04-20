627bc33e647e080a76e47af7bdd5532d35289a7a
Ciaran Evans <ciaran@developmentseed.org>
2021-04-20 14:03:06 +0100

    Run formatter, add Makefile

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,15 +15,15 @@ provider "azurerm" {
 
 locals {
   tags = {
-    "Project": "pangeo-forge-azure-bakery",
-    "Client": "Planetary Computer",
-    "Owner": var.owner,
-    "Stack": var.identifier
+    "Project" : "pangeo-forge-azure-bakery",
+    "Client" : "Planetary Computer",
+    "Owner" : var.owner,
+    "Stack" : var.identifier
   }
 }
 
 resource "azurerm_resource_group" "bakery_resource_group" {
   name     = "${var.identifier}-resource-group"
   location = var.region
-  tags = local.tags
+  tags     = local.tags
 }
