0fbf4d7cd03983112cfda5218662ab2a421c4e31
Ciaran Evans <ciaran@developmentseed.org>
2021-04-23 14:11:00 +0100

    Begin to setup base example of a manual flow for K8s

---
+++ flow_test/manual_flow.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+import prefect
+from prefect import Flow, storage, task
+from prefect.run_configs import KubernetesRun
+
+project = os.environ["PREFECT_PROJECT"]
+flow_name = "hello-flow"
+
+
+@task
+def say_hello():
+    logger = prefect.context.get("logger")
+    logger.info("Hello, Cloud")
+    return "hello result"
+
+
+with Flow(
+    flow_name,
+    run_config=KubernetesRun(
+        image="prefecthq/prefect:0.14.16-python3.8",
+        labels=json.loads(os.environ["PREFECT__CLOUD__AGENT__LABELS"]),
+    ),
+    storage=storage.Azure(container=os.environ["FLOW_STORAGE_CONTAINER"], connection_string=os.environ["FLOW_STORAGE_CONNECTION_STRING"]),
+) as flow:
+    hello_result = say_hello()
+
+flow.register(project_name=project)
