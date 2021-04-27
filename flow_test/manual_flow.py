a4493df26068c609d5d49093b60ee75f56f52696
Ciaran Evans <ciaran@developmentseed.org>
2021-04-27 11:58:23 +0100

    Add AZURE_STORAGE_CONNECTION_STRING as env var to KubernetesRun

--- flow_test/manual_flow.py
+++ flow_test/manual_flow.py
@@ -20,12 +20,13 @@ with Flow(
     flow_name,
     run_config=KubernetesRun(
         image="prefecthq/prefect:0.14.16-python3.8",
+        env={"AZURE_STORAGE_CONNECTION_STRING": os.environ["FLOW_STORAGE_CONNECTION_STRING"]},
         labels=json.loads(os.environ["PREFECT__CLOUD__AGENT__LABELS"]),
     ),
     storage=storage.Azure(
         container=os.environ["FLOW_STORAGE_CONTAINER"],
-        connection_string=os.environ["FLOW_STORAGE_CONNECTION_STRING"]
-    )
+        connection_string=os.environ["FLOW_STORAGE_CONNECTION_STRING"],
+    ),
 ) as flow:
     hello_result = say_hello()
 
