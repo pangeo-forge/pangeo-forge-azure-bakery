92176146c6c555ceed32dfd71511b6eff2b6c029
Ciaran Evans <ciaran@developmentseed.org>
2021-05-07 17:13:00 +0100

    Remove logging lines

--- flow_test/transform_tasks/xarray.py
+++ flow_test/transform_tasks/xarray.py
@@ -2,7 +2,6 @@ from typing import Any, List, Tuple
 
 import fsspec
 import xarray as xr
-import prefect
 from pangeo_forge_recipes import utils
 from prefect import task
 
@@ -16,14 +15,9 @@ def chunk(sources: List[Any], size: int) -> List[Tuple[Any, ...]]:
 def combine_and_write(
     sources: List[str], target: str, append_dim: str, concat_dim: str
 ) -> List[str]:
-    logger = prefect.context.get("logger")
-    logger.info("Starting combine and write")
     double_open_files = [fsspec.open(url).open() for url in sources]
-    logger.info("Double opened files")
     ds = xr.open_mfdataset(double_open_files, combine="nested", concat_dim=concat_dim)
-    logger.info("Xarray opened mfdataset")
     ds = ds.chunk({append_dim: len(sources)})
-    logger.info("Chunked dataset")
     mapper = fsspec.get_mapper(target)
 
     if not len(mapper):
