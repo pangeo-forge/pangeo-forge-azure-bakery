60e7830eebd11909d5d9ba8a6a392bcb7c09dbf9
Ciaran Evans <ciaran@developmentseed.org>
2021-05-10 11:50:39 +0100

    Debugging failing flow

--- flow_test/transform_tasks/xarray.py
+++ flow_test/transform_tasks/xarray.py
@@ -1,7 +1,9 @@
+import traceback
 from typing import Any, List, Tuple
 
 import fsspec
 import xarray as xr
+import prefect
 from pangeo_forge_recipes import utils
 from prefect import task
 
@@ -15,14 +17,21 @@ def chunk(sources: List[Any], size: int) -> List[Tuple[Any, ...]]:
 def combine_and_write(
     sources: List[str], target: str, append_dim: str, concat_dim: str
 ) -> List[str]:
-    double_open_files = [fsspec.open(url).open() for url in sources]
-    ds = xr.open_mfdataset(double_open_files, combine="nested", concat_dim=concat_dim)
-    ds = ds.chunk({append_dim: len(sources)})
-    mapper = fsspec.get_mapper(target)
+    try:
+        double_open_files = [fsspec.open(url).open() for url in sources]
+        ds = xr.open_mfdataset(double_open_files, combine="nested", concat_dim=concat_dim)
+        ds = ds.chunk({append_dim: len(sources)})
+        mapper = fsspec.get_mapper(target)
 
-    if not len(mapper):
-        kwargs = dict(mode="w")
-    else:
-        kwargs = dict(mode="a", append_dim=append_dim)
-    ds.to_zarr(mapper, **kwargs)
-    return target
+        if not len(mapper):
+            kwargs = dict(mode="w")
+        else:
+            kwargs = dict(mode="a", append_dim=append_dim)
+        ds.to_zarr(mapper, **kwargs)
+        return target
+    except Exception as ex:
+        logger = prefect.context.get("logger")
+        logger.info(ex)
+        logger.info(traceback.format_exc())
+        import time
+        time.sleep(60)
