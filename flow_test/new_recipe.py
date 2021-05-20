418b1246cb92eb3bd0cd32676a379e4535a7c390
Ciaran Evans <ciaran@developmentseed.org>
2021-05-20 13:33:11 +0100

    Use adlfs AzureBlobFileSystem

--- flow_test/new_recipe.py
+++ flow_test/new_recipe.py
@@ -7,6 +7,7 @@ from functools import wraps
 import azure.storage.blob
 import pandas as pd
 import prefect
+from adlfs import AzureBlobFileSystem
 from azure.core.exceptions import ResourceNotFoundError
 from dask_kubernetes.objects import make_pod_spec
 from pangeo_forge_recipes.patterns import pattern_from_file_sequence
@@ -16,64 +17,7 @@ from pangeo_forge_recipes.storage import FSSpecTarget
 from prefect import storage
 from prefect.executors.dask import DaskExecutor
 from prefect.run_configs.kubernetes import KubernetesRun
-from rechunker import executors
 from rechunker.executors import PrefectPipelineExecutor
-
-
-class AzureBlobStorageStore(MutableMapping):
-    def __init__(self, container_client, root=""):
-        logger = prefect.context.get("logger")
-        logger.info("ROOT IS:")
-        logger.info(root)
-        if len(root):
-            assert root[0] != "/"
-            assert root[-1] == "/"
-        self.container_client = container_client
-        self.root = root
-
-    def __getitem__(self, key):
-        key = os.path.join(self.root, key)
-        with self.container_client.get_blob_client(key) as bc:
-            try:
-                stream = bc.download_blob()
-            except ResourceNotFoundError as e:
-                raise KeyError(key) from e
-            data = stream.readall()
-        return data
-
-    def __setitem__(self, key, value):
-        key = os.path.join(self.root, key)
-        # bug in zarr? xarray?
-        if hasattr(value, "size") and value.size == 1 and hasattr(value, "tobytes"):
-            value = value.tobytes()
-
-        with self.container_client.get_blob_client(key) as bc:
-            bc.upload_blob(value, overwrite=True)
-
-    def __delitem__(self, key):
-        key = os.path.join(self.root, key)
-        with self.container_client.get_blob_client(key) as bc:
-            bc.delete_blob()
-
-    def __iter__(self):
-        prefix_len = len(self.root)
-        return (
-            x["name"][prefix_len:] for x in self.container_client.list_blobs(self.root)
-        )
-
-    def __len__(self):
-        return len(list(self.container_client.list_blobs(self.root)))
-
-
-class AzureBlobStorageFS:
-    def __init__(self, container_client):
-        self.container_client = container_client
-
-    def get_mapper(self, root):
-        return AzureBlobStorageStore(self.container_client, root)
-
-    def isdir(self, path):
-        return True
 
 
 def set_log_level(func):
@@ -90,13 +34,13 @@ def set_log_level(func):
 
 
 def register_recipe(recipe: BaseRecipe):
-    cc = azure.storage.blob.ContainerClient.from_connection_string(
-        os.environ["FLOW_STORAGE_CONNECTION_STRING"],
-        os.environ["FLOW_STORAGE_CONTAINER"],
+    fs_remote = AzureBlobFileSystem(
+        connection_string=os.environ["FLOW_STORAGE_CONNECTION_STRING"]
     )
-    fs_remote = AzureBlobStorageFS(cc)
     target = FSSpecTarget(fs_remote, root_path="azurerecipetest/")
     recipe.target = target
+    recipe.input_cache = FSSpecTarget(fs_remote, root_path="azurerecipetestcache/")
+    recipe.metadata_cache = target
 
     executor = PrefectPipelineExecutor()
     pipeline = recipe.to_pipelines()
