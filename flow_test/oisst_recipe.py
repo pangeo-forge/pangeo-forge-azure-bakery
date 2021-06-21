df5118dac1300e6470bf44cec84e8dfee007d79b
Tom Augspurger <taugspurger@microsoft.com>
2021-06-21 08:10:54 -0500

    wi

--- flow_test/oisst_recipe.py
+++ flow_test/oisst_recipe.py
@@ -93,17 +93,16 @@ def register_recipe(recipe: BaseRecipe):
     flow.register(project_name=project_name)
 
 
-def format_function(time):
-    base = pd.Timestamp("1981-09-01")
-    day = base + pd.Timedelta(days=time)
-    input_url_pattern = (
-        "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation"
-        "/v2.1/access/avhrr/{day:%Y%m}/oisst-avhrr-v02r01.{day:%Y%m%d}.nc"
-    )
-    return input_url_pattern.format(day=day)
-
-
 if __name__ == "__main__":
+    def format_function(time):
+        base = pd.Timestamp("1981-09-01")
+        day = base + pd.Timedelta(days=time)
+        input_url_pattern = (
+            "https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation"
+            "/v2.1/access/avhrr/{day:%Y%m}/oisst-avhrr-v02r01.{day:%Y%m%d}.nc"
+        )
+        return input_url_pattern.format(day=day)
+
     dates = pd.date_range("1981-09-01", "2021-01-05", freq="D")
     pattern = FilePattern(format_function, ConcatDim("time", range(len(dates)), 1))
     recipe = XarrayZarrRecipe(pattern, inputs_per_chunk=20, cache_inputs=True)
