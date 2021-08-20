5afa737cdf44b72de76388bcde71117e90bc4c8d
Alex Bush <alexbush@developmentseed.org>
2021-08-20 15:08:45 +0100

    refactor: tidy and split terraform

---
+++ terraform/supporting.tf
@@ -0,0 +1,22 @@
+locals {
+  tags = {
+    "Project" : "pangeo-forge-azure-bakery",
+    "Client" : "Planetary-Computer",
+    "Owner" : var.owner,
+    "Stack" : var.identifier
+  }
+}
+
+resource "azurerm_resource_group" "bakery_resource_group" {
+  name     = "${var.identifier}-bakery-resource-group"
+  location = var.region
+  tags     = local.tags
+}
+
+resource "azurerm_log_analytics_workspace" "bakery_logs_workspace" {
+  name                = "${var.identifier}-bakery-logs-workspace"
+  location            = azurerm_resource_group.bakery_resource_group.location
+  resource_group_name = azurerm_resource_group.bakery_resource_group.name
+  retention_in_days   = 30
+  tags                = local.tags
+}
