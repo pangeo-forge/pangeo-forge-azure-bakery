5413d1b93c6062d34bc816b266b199f8bfc7ca48
Alex Bush <alexbush@developmentseed.org>
2021-08-25 12:05:44 +0100

    fix: remove log analytics from readme

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -59,3 +59,29 @@ resource "azurerm_storage_container" "bakery_flow_cache_container" {
   storage_account_name  = azurerm_storage_account.bakery_flow_storage_account.name
   container_access_type = "private"
 }
+
+resource "azurerm_kubernetes_cluster" "bakery_cluster" {
+  name                = "${var.identifier}-bakery-cluster"
+  location            = azurerm_resource_group.bakery_resource_group.location
+  resource_group_name = azurerm_resource_group.bakery_resource_group.name
+  dns_prefix          = "${var.identifier}-bakery-cluster"
+
+  default_node_pool {
+    name                = "default"
+    max_count           = 100
+    min_count           = 1
+    vm_size             = "Standard_D2_v2"
+    os_disk_size_gb     = 30
+    enable_auto_scaling = true
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = local.tags
+}
