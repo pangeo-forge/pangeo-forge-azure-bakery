29dcef7ae53fe5d20e5180ab2d1df01a40a3e64d
Alex Bush <alexbush@developmentseed.org>
2021-08-25 16:12:09 +0100

    feat: remove superfluous log analytics workspace

--- terraform/supporting.tf
+++ terraform/supporting.tf
@@ -3,11 +3,3 @@ resource "azurerm_resource_group" "bakery_resource_group" {
   location = var.region
   tags     = local.tags
 }
-
-resource "azurerm_log_analytics_workspace" "bakery_logs_workspace" {
-  name                = "${var.identifier}-bakery-logs-workspace"
-  location            = azurerm_resource_group.bakery_resource_group.location
-  resource_group_name = azurerm_resource_group.bakery_resource_group.name
-  retention_in_days   = 30
-  tags                = local.tags
-}
