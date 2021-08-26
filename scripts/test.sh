9b089d8cc432125ac5260be0d5de01bebf86431e
Alex Bush <alexbush@developmentseed.org>
2021-08-26 14:59:31 +0100

    fix: test code

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-poetry run dotenv run sh -c 'docker run -it --rm \
--v $$(pwd)/test/recipes/$(flow):/$(flow) \
--e FLOW_STORAGE_CONNECTION_STRING -e FLOW_STORAGE_CONTAINER -e FLOW_CACHE_CONTAINER -e BAKERY_IMAGE \
-  -e PREFECT__CLOUD__AGENT__LABELS -e PREFECT_PROJECT -e PREFECT__CLOUD__AUTH_TOKEN \
-  $$BAKERY_IMAGE python3 /$(flow)'
+poetry run dotenv run sh -c 'FLOW=oisst_recipe.py &&
+docker run -it --rm \
+  -v $(pwd)/test/recipes/$FLOW:/$FLOW \
+  -e FLOW_STORAGE_CONNECTION_STRING \
+  -e FLOW_STORAGE_CONTAINER \
+  -e FLOW_CACHE_CONTAINER -e BAKERY_IMAGE \
+  -e PREFECT__CLOUD__AGENT__LABELS \
+  -e PREFECT_PROJECT \
+  -e PREFECT__CLOUD__AUTH_TOKEN \
+  $BAKERY_IMAGE python3 /$FLOW'
