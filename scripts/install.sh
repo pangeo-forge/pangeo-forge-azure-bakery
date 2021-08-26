85e87b0603a927c3e0a062870da8dbc999157636
Alex Bush <alexbush@developmentseed.org>
2021-08-26 12:29:36 +0100

    fix: install script; fix: k8s connect script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,5 @@ poetry install
 poetry run dotenv run terraform -chdir="terraform/" plan
 poetry run dotenv run terraform -chdir="terraform/" apply -auto-approve
 ./scripts/k8s-connect.sh
-poetry run dotenv run sh -c 'kubectl create namespace $$BAKERY_NAMESPACE --dry-run=client -o yaml | kubectl apply -f - && cat kubernetes/prefect_agent_conf.yaml | envsubst | kubectl apply -f -'
+poetry run dotenv run sh -c 'kubectl create namespace $BAKERY_NAMESPACE --dry-run=client -o yaml | kubectl apply -f - && cat kubernetes/prefect_agent_conf.yaml | envsubst | kubectl apply -f -'
 poetry run dotenv run bash ./scripts/retrieve_flow_storage_values.sh
